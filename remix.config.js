3f8837181cf0dc5da27914a285ae7a6b929053e2
6plusjp <6plusjp@gmail.com>
2022-05-13 16:43:28 +0900

    init

--- remix.config.js
+++ remix.config.js
@@ -12,24 +12,5 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
-  serverDependenciesToBundle: [
-    'decode-named-character-reference',
-    'character-entities',
-    'ccount',
-    'bail',
-    'trough',
-    'vfile',
-    'vfile-message',
-    'property-information',
-    'space-separated-tokens',
-    'comma-separated-tokens',
-    'vfile-location',
-    'web-namespaces',
-    'zwitch',
-    'html-void-elements',
-    'stringify-entities',
-    'character-entities-legacy',
-    'character-entities-html4',
-    'html-whitespace-sensitive-tag-names'
-  ]
+  serverDependenciesToBundle: []
 }
